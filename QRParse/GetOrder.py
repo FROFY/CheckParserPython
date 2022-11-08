ea0691354b87d15f7d73473721892d7580ad78ba
Kukushkin Ilya Alekseevich <Kukushkin_IA@stack.stack-it.ru>
2022-11-08 16:16:10 +0300

    Распихал по директориям для тестов

---
+++ QRParse/GetOrder.py
@@ -0,0 +1,20 @@
+import requests
+
+url = 'https://proverkacheka.com/api/v1/check/get'
+token = '17229.R0nyP64mqKF7EaTHZ'
+
+
+def print_hi(qr_string):
+    data = {
+        'token': token,
+        'qrraw': qr_string
+    }
+    response = requests.post(url=url, data=data)
+    items = response.json()
+    item = items["data"]["json"]["items"][0]["name"]
+    price = items["data"]["json"]["items"][0]["price"] / 100
+    count = items["data"]["json"]["items"][0]["quantity"]
+    return item, price, count
+    # print('Товар: ' + item)
+    # print('Цена: ' + str(price) + 'р')
+    # print('Количество: ' + str(count))
--- main.py
+++ main.py
@@ -1,24 +1,6 @@
 # -*- coding: utf-8 -*-
-
-import json
-
-import requests
-
-from Deserialize import Root
-
-
-def print_hi(qr_string):
-    url = 'https://proverkacheka.com/api/v1/check/get'
-    data = {
-        'token': '17229.R0nyP64mqKF7EaTHZ',
-        'qrraw': qr_string
-    }
-    response = requests.post(url=url, data=data)
-    item = response.json()["data"]["json"]["items"][0]["name"]
-    price = response.json()["data"]["json"]["items"][0]["price"] / 100
-    print('Товар: ' + item)
-    print('Цена: ' + str(price))
-
+from QRParse import GetOrder
 
 if __name__ == '__main__':
-    print_hi('t=20221102T1352&s=89.99&fn=9960440301834413&i=6073&fp=1929603051&n=1')
+    orders = GetOrder.print_hi('t=20221102T1352&s=89.99&fn=9960440301834413&i=6073&fp=1929603051&n=1')
+    print(f'Название: {orders[0]}\nКоличество: {orders[2]}\nЦена: {orders[1]}р')
